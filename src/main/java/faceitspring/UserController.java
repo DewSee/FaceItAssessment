ac6d2ac2fcb7140937bd5dc89cb9ced956eceb86
DewSee <veckler08@gmail.com>
2019-01-09 22:11:15 +0100

    add user filtering by country

--- src/main/java/faceitspring/UserController.java
+++ src/main/java/faceitspring/UserController.java
@@ -1,12 +1,17 @@
 package faceitspring;
 
 import java.util.List;
+import java.util.Optional;
+import static java.util.stream.Collectors.toList;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,50 +23,53 @@ public class UserController {
         this.repository = repository;
     }
 
-    @GetMapping("/users")
+    @RequestMapping(value = "/users", method = RequestMethod.GET)
     List<User> allUsers() {
         return repository.findAll();
     }
 
-    @PostMapping("/users")
-    User newUser(@RequestBody User newUser) {
-        return repository.save(newUser);
-    }
-
-    @GetMapping("/users/{nickName}")
-    User oneUser(@PathVariable String nickName) {
-
-        return repository.findById(nickName)
-                .orElseThrow(() -> new UserNotFoundException(nickName));
-    }
-
-    @PutMapping("/users/{nickName}")
-    User replaceUser(@RequestBody User newUser, @PathVariable String nickName) {
-
-        return repository.findById(nickName)
-                .map(user -> {
-                    user.setFirstName(newUser.getFirstName());
-                    user.setNickName(newUser.getLastName());
-                    user.setNickName(newUser.getNickName());
-                    user.setPassword(newUser.getPassword());
-                    user.setEmail(newUser.getEmail());
-                    user.setCountry(newUser.getCountry());
-                    return repository.save(user);
-                })
-                .orElseGet(() -> {
-                    newUser.setNickName(nickName);
-                    return repository.save(newUser);
-                });
-    }
-
-    @DeleteMapping("/users/{nickName}")
-    void deleteUser(@PathVariable String nickName) {
-        repository.deleteById(nickName);
-    }
-
-//    @GetMapping("/users/{nickName}")
-//    List<User> specificUsers(@PathVariable String nickName) {
-//
-//        return repository.find
+//    @GetMapping("/users")
+//    List<User> allUsers() {
+//        return repository.findAll();
 //    }
+//    @PostMapping("/users")
+//    User newUser(@RequestBody User newUser) {
+//        return repository.save(newUser);
+//    }
+//
+//    @GetMapping("/users/{nickName}")
+//    User oneUser(@PathVariable String nickName) {
+//
+//        return repository.findById(nickName)
+//                .orElseThrow(() -> new UserNotFoundException(nickName));
+//    }
+//
+//    @PutMapping("/users/{nickName}")
+//    User replaceUser(@RequestBody User newUser, @PathVariable String nickName) {
+//
+//        return repository.findById(nickName)
+//                .map(user -> {
+//                    user.setFirstName(newUser.getFirstName());
+//                    user.setNickName(newUser.getLastName());
+//                    user.setNickName(newUser.getNickName());
+//                    user.setPassword(newUser.getPassword());
+//                    user.setEmail(newUser.getEmail());
+//                    user.setCountry(newUser.getCountry());
+//                    return repository.save(user);
+//                })
+//                .orElseGet(() -> {
+//                    newUser.setNickName(nickName);
+//                    return repository.save(newUser);
+//                });
+//    }
+//
+//    @DeleteMapping("/users/{nickName}")
+//    void deleteUser(@PathVariable String nickName) {
+//        repository.deleteById(nickName);
+//    }
+    @RequestMapping(value = "users/{country}", method = RequestMethod.GET)
+    List<User> findByCountry(@PathVariable String country) {
+        return repository.findAll().stream().filter(u -> u.getCountry().equals(country)).collect(toList());
+
+    }
 }
